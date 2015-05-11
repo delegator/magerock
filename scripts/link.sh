41b97858eeaea20af62db165124029cb77e61459
Will Lewis <will.l@delegator.com>
2015-05-11 12:04:04 -0400

    Fancies up the link script; runs link script after composer install command

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,9 +1,33 @@
+#!/bin/bash
+tput setaf 4; echo ": Starting to link Magento Core"
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-mkdir $DIR/../web/magento/
-pushd $DIR/../web/magento/
 
-ln -s ../../vendor/magento/core/* ./
-modman init
-modman link ../theme
+if [ ! -e "$DIR/../web/magento/" ]
+then
+  mkdir $DIR/../web/magento/
+fi
 
-popd
+pushd $DIR/../web/magento/ 1>/dev/null
+
+tput setaf 4; echo ": Linking Core"
+if ln -s ../../vendor/magento/core/* ./ 2>/dev/null
+then
+  tput setaf 4; echo "  └─ Core successfully linked"
+else
+  tput setaf 3; echo "  └─ Core already linked"
+fi
+
+tput setaf 4; echo ": Initializing Modman"
+if [ -e ".modman" ]
+then
+  tput setaf 3; echo "  └─ Modman already initialized"
+else
+  tput setaf 4; printf "  ├─ "
+  modman init
+  tput setaf 4; printf "  └─ "
+  modman link ../theme
+fi
+
+popd 1>/dev/null
+tput setaf 4; echo ": Done"
