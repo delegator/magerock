6b237978ab02e3e1cb2ef88f2126e175d4d8c681
Will Lewis <will.l@delegator.com>
2015-05-13 11:32:46 -0400

    More debug

--- scripts/Installer.php
+++ scripts/Installer.php
@@ -92,7 +92,7 @@ class Installer
         echo `php --version`;
         echo `which php`;
         self::output("     └─ Installing");
-        $cmd = 'php -f install.php -- '
+        $cmd = '/usr/bin/local/php -f install.php -- '
             . '--license_agreement_accepted "yes" '
             . '--locale "'. getenv('LOCALE') . '" '
             . '--timezone "'. getenv('TIME_ZONE') . '" '
